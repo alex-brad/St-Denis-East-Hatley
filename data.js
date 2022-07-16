a0b2b5d69cbcae5978c15b4ff5ae1063eb5fba0b
alex-brad <alex.brad@live.co.uk>
2022-07-16 07:04:01 +0100

    Revert "Revert "!""
    This reverts commit acb439489fd0475f0696b61372c9249f76b9ad4c.

---
+++ data.js
@@ -0,0 +1,206 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-st-denis-east-hatley-hd",
+      "name": "Start",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.003654661149548488,
+          "pitch": 0.4405325696237501,
+          "rotation": 0,
+          "target": "1-st-denis-east-hatley-hd-mid"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.624621647348695,
+          "pitch": 0.25379919074508805,
+          "title": "<div>Tomb of JOHN SHIPSTON</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.0402561161677557,
+          "pitch": 0.03582076082968655,
+          "title": "Font",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.73363951775936,
+          "pitch": -0.07265930689676559,
+          "title": "William Cray Say<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.3099013742097352,
+          "pitch": 0.24021647168198612,
+          "title": "<span style=\"color: rgb(32, 33, 36); font-family: Roboto, Arial, sans-serif; font-variant-ligatures: none; letter-spacing: 0.1px; white-space: pre-wrap; background-color: rgb(255, 255, 255);\">'Tortoise' Stove</span>",
+          "text": "text"
+        },
+        {
+          "yaw": -1.6273835398046508,
+          "pitch": -0.08215274798015137,
+          "title": "North Door",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-st-denis-east-hatley-hd-mid",
+      "name": "Nave",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.133414213568507,
+          "pitch": 0.5283796594056227,
+          "rotation": 0,
+          "target": "0-st-denis-east-hatley-hd"
+        },
+        {
+          "yaw": -0.007149059539028002,
+          "pitch": 0.37355258650968537,
+          "rotation": 0,
+          "target": "2-st-denis-east-hatley-hd-top"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.830051502856449,
+          "pitch": 0.15366280805739763,
+          "title": "Pulpit",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.5384178706579803,
+          "pitch": 0.623888729412748,
+          "title": "<span style=\"color: rgb(0, 0, 0); font-family: Arial, sans-serif; background-color: rgb(255, 255, 255);\">Castell Tombs</span><br>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 1.3538869916198717,
+          "pitch": 0.06746842961487687,
+          "title": "Piscina",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.9778517455703284,
+          "pitch": 0.1909418383635142,
+          "title": "Chancel",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-st-denis-east-hatley-hd-top",
+      "name": "Chancel",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1129943545380296,
+          "pitch": 0.39653741170085155,
+          "rotation": 0,
+          "target": "1-st-denis-east-hatley-hd-mid"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0290055556745799,
+          "pitch": 0.03778138662385189,
+          "title": "Glass",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.34551346266372107,
+          "pitch": 0.14921472713032102,
+          "title": "Altar",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.0675106713328884,
+          "pitch": 0.043485221531279095,
+          "title": "Vestry",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "St Denis, East Hatley",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
